d72744013b6467280493b0cdcbed58b2ab920e1d
kkang <kkang104@gmail.com>
2025-09-30 14:40:06 +0900

    Add files via upload

--- ch8/Test.kt
+++ ch8/Test.kt
@@ -27,4 +27,45 @@ fun main() {
     //멤버접근........................................
     val length = data1.length
     val length2 = data2?.length//nullable 객체의 멤버 접근은 ?.
+    println(length2)//null
+    data2 = "hello"
+    val length3 = data2?.length
+    println(length3)//5
+
+
+    //일치 연산자....................................................
+    //일반 객체(기초데이터 객체가 아닌) ==, === 의 차이가 없다..
+    //모두 객체 비교이다..
+    //자바로 변형되도 객체이다.. 모두 객체 비교..
+    class User
+
+    val user1 = User()
+    val user2 = User()
+    println("${user1 == user2}, ${user1 === user2}")//false, false
+
+    //기초데이터 객체 - non null
+    //==, === 차이가 없다.. 모두 값 비교..
+    //자바로 변형시에 primitive type으로 변형된다.. 객체가 아니다.. 값 비교 할수 밖에 없다..
+    val a1: Int = 1000
+    val a2: Int = 1000
+    println("${a1 == a2}, ${a1 === a2}")//true, true
+
+    //기초데이터 객체 - nullable
+    //== 값비교.., === 객체비교..
+    //nullable 선언하면 런타임시점에 null대입이 될 수도 있다. 자바 변형시.. Integer 로 변경된다..
+    //기초 타입을 객체로 표현하기 위해서 int -> Integer, double -> Double 등을 Wrapper 클래스라 부른다.
+    val b1: Int? = 1000
+    val b2: Int? = 1000
+    println("${b1 == b2}, ${b1 === b2}")//true, false
 }
+
+
+
+
+
+
+
+
+
+
+
